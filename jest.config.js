5c892bcfb4f1d5efe4a3f61a2e1b5533fbf3efa9
Sam Liu <yu.yang.sam.liu@gmail.com>
2020-01-18 13:28:23 +0800

    chore: Add dev config

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: [
+    'ts',
+    'tsx',
+    'js',
+    'jsx',
+    'json',
+    'node'
+  ],
+  globals: {
+    'ts-jest': {
+      isolatedModules: true
+    }
+  }
+}
+  
