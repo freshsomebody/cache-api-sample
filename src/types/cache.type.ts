c0a9b0585c2d63c1b703b2307de6b5fa790dfb04
Sam Liu <yu.yang.sam.liu@gmail.com>
2020-01-29 17:01:05 +0800

    chore: Add types for Cache get method

--- src/types/cache.type.ts
+++ src/types/cache.type.ts
@@ -2,3 +2,15 @@ export interface CacheDoc {
   key: string,
   value: any
 }
+
+export interface CacheGetOptions {
+  strategy?: CacheStrategies
+}
+
+export enum CacheStrategies {
+  CacheFirst,
+  NetworkFirst,
+  CacheOnly,
+  NetworkOnly,
+  StaleWhileRevalidate
+}
